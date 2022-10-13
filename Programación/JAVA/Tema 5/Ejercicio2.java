81d8d080f1a0af19e13099c8df0f5d279ff931ab
RomeoDb17 <romeodominguez28@gmail.com>
2022-10-13 10:36:33 +0200

    deleted:    Entornos de Desarrollo/README.MD 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio1.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio10.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio11.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio12.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio13.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio14.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio15.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio16.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio17.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio18.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio2.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio3.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio4.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio5.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio6.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio7.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio8.java" 	new file:   "Programaci\303\263n/JAVA/Tema 5/Ejercicio9.java"

---
+++ Programación/JAVA/Tema 5/Ejercicio2.java
@@ -0,0 +1,12 @@
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int i = 0;
+        
+        while(i <= 100) {
+          System.out.println(i);
+          i+=5;
+        }
+      }
+}
+
+// Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
---
+++ Programación/JAVA/Tema 5/Ejercicio6.java
@@ -0,0 +1,12 @@
+public class Ejercicio6 {
+    public static void main(String[] args) {
+        int i = 320;
+        
+        do {
+        System.out.println(i);
+        i-=20;
+        } while(i >= 160);
+  }
+}
+
+// Muestra los números del 320 al 160, contando de 20 en 20 utilizando un bucle do-while.
